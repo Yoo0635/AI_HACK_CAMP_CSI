59b27dc6627cebec94f0c97a5979042159f5e0f3
composedly13 <composedly13@gmail.com>
2026-05-16 12:21:09 +0900

    feat: set FastAPI URL and NODE_ID from network config

--- include/config.h
+++ include/config.h
@@ -3,5 +3,5 @@
 #define WIFI_SSID       "your_ssid"
 #define WIFI_PASSWORD   "your_password"
 
-#define NODE_ID         "esp32s3-node-01"
-#define FASTAPI_URL     "http://192.168.0.0:8000/csi/log"
+#define NODE_ID         "esp32s3-4712D0"
+#define FASTAPI_URL     "http://192.168.1.2:8000/csi/log"
